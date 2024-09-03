080f42d85e825404c8a6b061d37c54cdf115927d
David Kachanov <davidkachaanov2004@gmail.com>
2024-09-03 21:29:39 +0300

    добавили яблоко и статичную змейку

--- snake.js
+++ snake.js
@@ -2,12 +2,12 @@
 var boardWidth = 1280;
 var boardHeight = 800;
 
-//скорость змейки
-var snakeSpeed = 125
 class Snake{
-    constructor(left,top){
+    constructor(left, top){
         this.left=Math.round(left);
         this.top=Math.round(top);
+        //скорость змейки
+        this.speed=125;
     }
     render(){
         let result = document.createElement('div');
@@ -17,14 +17,34 @@ class Snake{
         return result;
     }
 }
+class Apple{
+    constructor(left, top){
+        this.left=Math.round(left);
+        this.top=Math.round(top);
+    }
+    render(){
+        let result = document.createElement('div');
+        result.setAttribute('class', 'apple');
+        result.style.left=this.left + 'px';
+        result.style.top=this.top + 'px';
+        return result;
+    }
+}
 
-function addsnake (){
+function addobjects (){
     let snake = new Snake(
-        Math.random() * boardWidth, 
-        Math.random() * boardHeight 
+        (boardWidth - 20) / 2, 
+        (boardHeight - 20) / 2
+    );
+    let apple = new Apple(
+        Math.random() * (boardWidth - 20), 
+        Math.random() * (boardHeight - 20)
     );
     gameBoard.appendChild(
-    snake.render()    
+        snake.render()    
+    );
+    gameBoard.appendChild(
+        apple.render()    
     );
 }
 
@@ -42,7 +62,7 @@ function changeDirection(keyCode) {
 
 window.addEventListener(
     'load',
-    addsnake
+    addobjects
 );
 
 
